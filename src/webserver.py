02a0d3c49dcf1d6d29dabe511b52a599170323cc
KimSickal <shihwan1004@naver.com>
2018-12-17 10:54:48 +0900

    simple query handling

--- src/webserver.py
+++ src/webserver.py
@@ -1,19 +1,44 @@
+import threading
+import time
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import urlparse
 
-class handler(BaseHTTPRequestHandler):
+flag = False
+shutdown = False
+
+def LEDHandler():
+	while(not shutdown):
+		print flag
+		time.sleep(1)
+	
+
+class httpHandler(BaseHTTPRequestHandler):
 	def do_GET(self):
-		print 'asdf'
+		print 'ping' + str(self.path)
 		self.send_response(200)
 		self.send_header('Content-type', 'text/html')
 		self.end_headers()
-		self.wfile.write("Hello, World!")
-		return
+		self.wfile.write(''+str(flag))
+
+		pathParam = self.path[1]
+
+		global flag
+
+		if(pathParam == 't'):
+			flag = True
+		elif(pathParam == 'f'):
+			flag = False
 
 try:
-	server = HTTPServer(('', 8080), handler)
-	print 'Started httpserver on pirt 8080'
+	server = HTTPServer(('', 8080), httpHandler)
+	print 'Started httpserver on port 8080'
+	LEDThread = threading.Thread(target=LEDHandler)
+	# LEDThread.daemon = True
+	LEDThread.start()
 	server.serve_forever()
 
 except KeyboardInterrupt:
 	print '^C'
+	global shutdown
+	shutdown = True
 	server.socket.close()
