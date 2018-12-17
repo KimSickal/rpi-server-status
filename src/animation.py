66c33c327a1e4f3585eae92e049a6403ae85b571
KimSickal <shihwan1004@naver.com>
2018-12-17 16:37:33 +0900

    애니메이션 show disable 구현

--- src/animation.py
+++ src/animation.py
@@ -1,20 +1,72 @@
+import time
+
 from Adafruit_LED_Backpack import BicolorMatrix8x8
 
+from PIL import Image
+from PIL import ImageDraw
 
-display = BicolorMatrix8x8.BicolorMatrix8x8()
-display.begin()
-display.clear()
+RED = (255, 0, 0)
+GRN = (0, 255, 255)
+YEL = (255, 255, 0)
+BLK = (0, 0, 0)
 
-for c in [BicolorMatrix8x8.RED, BicolorMatrix8x8.GREEN, BicolorMatrix8x8.YELLOW]:
-    # Iterate through all positions x and y.
-    for x in range(8):
-        for y in range(8):
-            # Clear the display buffer.
-            display.clear()
-            # Set pixel at position i, j to appropriate color.
-            display.set_pixel(x, y, c)
-            # Write the display buffer to the hardware.  This must be called to
-            # update the actual display LEDs.
-            display.write_display()
-            # Delay for a quarter second.
-            time.sleep(0.25)
+bitmapImage = {
+	'success': [
+		[BLK, BLK, GRN, GRN, GRN, GRN, BLK, BLK, ],
+		[BLK, GRN, GRN, GRN, GRN, GRN, GRN, BLK, ],
+		[GRN, GRN, GRN, GRN, GRN, GRN, GRN, GRN, ],
+		[GRN, GRN, GRN, GRN, GRN, GRN, GRN, GRN, ],
+		[GRN, GRN, GRN, GRN, GRN, GRN, GRN, GRN, ],
+		[GRN, GRN, GRN, GRN, GRN, GRN, GRN, GRN, ],
+		[BLK, GRN, GRN, GRN, GRN, GRN, GRN, BLK, ],
+		[BLK, BLK, GRN, GRN, GRN, GRN, BLK, BLK, ],
+	]
+}
+
+class matrix (object):
+	def __init__(self):
+		self.display = BicolorMatrix8x8.BicolorMatrix8x8()
+		self.frameIndex = 0
+		self.currMode = ''
+		self.display.begin()
+		self.display.clear()
+
+	def _showImage(self, image, fps, duration):
+		if(duration < 0):
+			self.display.set_image(image)
+			self.frameIndex = 0
+		elif(self.frameIndex < duration):
+			self.display.set_image(image)
+			self.frameIndex += 1
+		else:
+			self.display.create_blank_image()
+			self.frameIndex = 0
+		self.display.write_display()
+		time.sleep(1000 / fps)
+
+	def _initImage(self, image, fps, duration):
+		if(duration == 0):
+			self.display.create_blank_image()
+		else:
+			self.display.set_image(image)
+		self.frameIndex = 0
+		self.display.write_display()
+		time.sleep(1000 / fps)
+
+	def disableImage(self):
+		self.display.create_blank_image()
+		self.display.write_display()
+		self.currMode = ''
+		self.frameIndex = 0
+
+	def playImage(self, imageName, fps = 1, duration = -1):
+		if(self.currMode == imageName):
+			self._showImage(bitmapImage[imageName], fps, duration)
+		else:
+			self.currMode = imageName
+			self._initImage(bitmapImage[imageName], fps, duration)
+
+
+
+
+			
